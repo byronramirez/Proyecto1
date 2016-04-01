360e5b014c9b7970b201299cbdc43d2bdf50de2e
byronramirez <byronjalapa96@gmail.com>
2016-03-31 21:27:03 -0600

    Added files via upload

---
+++ Archivos Secuenciales.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int Base, Altura;
+	float Area;
+
+	cout << "Ingrese Base: ";
+	cin >> Base;
+	cout << "Ingrese Altura: ";
+	cin >> Altura;
+
+	Area=Base*Altura/2;
+
+	cout << "EL AREA ES: " << endl << Area;
+		return 0;
+}
